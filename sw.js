0f91474f4f4eb6c359651eacf5d60b1b639fcae5
webnian <919882905@qq.com>
2019-10-01 12:04:15 +0800

    Create CNAME

--- sw.js
+++ sw.js
@@ -44,3 +44,12 @@ workbox.routing.registerRoute(
 		],
 	})
 );
+
+self.addEventListener('beforeinstallprompt', function(e) {
+	// 这个 `event.userChoice` 是一个 Promise ，在用户选择后 resolve
+	e.userChoice.then(result => {
+		console.log(result.outcome)
+		// 'accepted': 添加到主屏
+		// 'dismissed': 用户不想理你并向你扔了个取消
+	})
+})
