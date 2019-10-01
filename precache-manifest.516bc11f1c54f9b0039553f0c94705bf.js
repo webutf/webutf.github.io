f8d2842d639800199b4072c2324cf7dca837dda8
webnian <919882905@qq.com>
2019-10-01 17:00:37 +0800

    Create CNAME

---
+++ precache-manifest.516bc11f1c54f9b0039553f0c94705bf.js
@@ -0,0 +1,69 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e170ca776455ff10a092da76b7faa9e4",
+    "url": "CNAME"
+  },
+  {
+    "revision": "96c0812a89bfa930276e94a694d82d98",
+    "url": "assets/da.96c0812.jpg"
+  },
+  {
+    "revision": "96c0812a89bfa930276e94a694d82d98",
+    "url": "assets/da.jpg"
+  },
+  {
+    "revision": "07bdf265c031fc142c5292fa7d37957a",
+    "url": "assets/xiao.jpg"
+  },
+  {
+    "revision": "45800ba5b21347cd50e871209162b80c",
+    "url": "index.html"
+  },
+  {
+    "revision": "94b6ba6a8b66a683b99c",
+    "url": "main.23dd62db.js"
+  },
+  {
+    "revision": "94b6ba6a8b66a683b99c",
+    "url": "main.846ecfd1.css"
+  },
+  {
+    "revision": "0698db28acdc46c546e728afb374deac",
+    "url": "mods/common/rem.js"
+  },
+  {
+    "revision": "b89488dbfb07e1597a4ecfee42cd71e1",
+    "url": "mods/common/utils.js"
+  },
+  {
+    "revision": "2f772fed444d5489079f275bd01e26cc",
+    "url": "mods/lib/jquery.min.js"
+  },
+  {
+    "revision": "f121238864e2a9ff7e97bf60b159feb0",
+    "url": "mods/lib/vue.min.js"
+  },
+  {
+    "url": "precache-manifest.7fcd8c6ee7fa55f57f2271c691767fff.js"
+  },
+  {
+    "revision": "adca2181ee19251d30f41200619d264a",
+    "url": "service-worker.js"
+  },
+  {
+    "revision": "51d3b3c8a7fdfe2f573057d7a96c04d9",
+    "url": "static/favicon.ico"
+  },
+  {
+    "revision": "a49fa3ee9b87cf68b701ab2453525eab",
+    "url": "static/manifest.json"
+  },
+  {
+    "revision": "d171434e291d1f746827e68992d08d94",
+    "url": "styles/index.less"
+  },
+  {
+    "revision": "8861ba364c901410ef2da1097d3a4062",
+    "url": "sw.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,18 +1,41 @@
+importScripts("precache-manifest.516bc11f1c54f9b0039553f0c94705bf.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 !function () { "use strict"; try { self["workbox:sw:4.3.1"] && _() } catch (t) { } const t = "https://storage.googleapis.com/workbox-cdn/releases/4.3.1", e = { backgroundSync: "background-sync", broadcastUpdate: "broadcast-update", cacheableResponse: "cacheable-response", core: "core", expiration: "expiration", googleAnalytics: "offline-ga", navigationPreload: "navigation-preload", precaching: "precaching", rangeRequests: "range-requests", routing: "routing", strategies: "strategies", streams: "streams" }; self.workbox = new class { constructor() { return this.v = {}, this.t = { debug: "localhost" === self.location.hostname, modulePathPrefix: null, modulePathCb: null }, this.s = this.t.debug ? "dev" : "prod", this.o = !1, new Proxy(this, { get(t, s) { if (t[s]) return t[s]; const o = e[s]; return o && t.loadModule(`workbox-${o}`), t[s] } }) } setConfig(t = {}) { if (this.o) throw new Error("Config must be set before accessing workbox.* modules"); Object.assign(this.t, t), this.s = this.t.debug ? "dev" : "prod" } loadModule(t) { const e = this.i(t); try { importScripts(e), this.o = !0 } catch (s) { throw console.error(`Unable to import module '${t}' from '${e}'.`), s } } i(e) { if (this.t.modulePathCb) return this.t.modulePathCb(e, this.t.debug); let s = [t]; const o = `${e}.${this.s}.js`, r = this.t.modulePathPrefix; return r && "" === (s = r.split("/"))[s.length - 1] && s.splice(s.length - 1, 1), s.push(o), s.join("/") } } }();
 //# sourceMappingURL=workbox-sw.js.map
 console.log(456465)
 var CACHE_NAME = "H5X_cache_v1.0.0";
+var cacheList = [
+	"vue.min.js",
+	"jquery.min.js",
+	"rem.js"
+]
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
 	console.log(fetch)
 })
 
-self.addEventListener('install', function(e) {
-	e.waitUntil(self.skipWaiting())
+self.addEventListener('install', function (e) {
+	e.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(cache => cache.addAll(cacheList))
+			.then(() => self.skipWaiting())
+	)
 })
 
-self.addEventListener('activate', function(e) {
-	e.waitUntil(clients.claim())
+self.addEventListener('activate', function (e) {
+	e.waitUntil(
+		Promise.all(
+			caches.keys().then(cacheNames => {
+				return cacheNames.map(name => {
+					if (name != CACHE_NAME) {
+						return caches.delete(name)
+					}
+				})
+			})
+		)
+	).then(() => {
+		return self.clients.claim()
+	})
 })
 
 if (workbox) {
@@ -25,9 +48,16 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-	/\.css/,
+	/^https:\/\/fonts\.googleapis\.com/,
 	new workbox.strategies.StaleWhileRevalidate({
-		cacheName: 'css-cache',
+		cacheName: CACHE_NAME,
+	}),
+);
+
+workbox.routing.registerRoute(
+	/\.(?:js|css|json)$/,
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: CACHE_NAME,
 	})
 );
 
@@ -35,17 +65,17 @@ workbox.routing.registerRoute(
 	/\.(?:png|jpg|jpeg|svg|gif)$/,
 	// Use the cache if it's available.
 	new workbox.strategies.CacheFirst({
-		cacheName: 'image-cache',
+		cacheName: CACHE_NAME,
 		plugins: [
 			new workbox.expiration.Plugin({
-				maxEntries: 20,
+				maxEntries: 60,
 				maxAgeSeconds: 7 * 24 * 60 * 60,
 			})
 		],
 	})
 );
 
-self.addEventListener('beforeinstallprompt', function(e) {
+self.addEventListener('beforeinstallprompt', function (e) {
 	// 这个 `event.userChoice` 是一个 Promise ，在用户选择后 resolve
 	e.userChoice.then(result => {
 		console.log(result.outcome)
