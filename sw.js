da34b2dd9da6d7b213ca28ce4986f574e5b95415
webnian <919882905@qq.com>
2019-10-02 10:07:43 +0800

    Create CNAME

--- sw.js
+++ sw.js
@@ -17,17 +17,42 @@ self.addEventListener('activate', function (e) {
 	e.waitUntil(
 		Promise.all([
 			clients.claim(),
-			caches.keys().then(cacheNames => {
-				return cacheNames.map(name => {
-					if (name != CACHE_NAME) {
-						return caches.delete(name)
-					}
-				})
+			caches.keys().then(keys => {
+				Promise.all(
+					keys.filter(key => !key.startsWith(CACHE_NAME))
+						.map(key => caches.delete(key))
+				)
 			})
 		])
 	)
 })
 
 self.addEventListener('fetch', function (e) {
-	
+	e.respondWith(
+		caches.match(e.request)
+			.then(hit => {
+				if (hit) return hit;
+
+				const fetchRequest = e.request.clone();
+				if (navigator.onLine) {
+					return onlineRequest(fetchRequest);
+				}
+			})
+	)
 })
+
+function onlineRequest(fetchRequest) {
+	return fetch(fetchRequest).then(response => {
+		if (!response || response.status !== 200
+			|| !response.headers.get('Content-type').match(/image|javascript|test\/css/i)) {
+			return response;
+		}
+		const responseToCache = response.clone();
+		caches.open(CACHE_NAME)
+			.then(function (cache) {
+				cache.put(fetchRequest.request, responseToCache);
+			});
+		return response;
+	}).catch(() => {
+	});
+}
