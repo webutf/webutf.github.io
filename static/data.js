89c0dea0daef8ec03467360ce426b677fd6cc6af
webutf <919882905@qq.com>
2020-03-03 13:41:17 +0800

    升级

--- static/data.js
+++ static/data.js
@@ -1,53 +1,16 @@
 var qianke = [
-  {
-    name: "XUI框架",
-    style: "",
-    href: "http://千克.com/xui"
-  },
-  {
-    name: "js函数宝典",
-    style: "",
-    href: "http://千克.com/baodian"
-  },
-  {
-    name: "Vue实践",
-    style: "",
-    href: "http://千克.com/vue"
-  },
-  {
-    name: "React实践",
-    style: "",
-    href: "http://千克.com/react"
-  },
-  {
-    name: "npm包源码解读",
-    href: "http://千克.com/npm"
-  }
+  { name: "XUI框架", href: "http://千克.com/xui" },
+  { name: "js函数宝典", href: "http://千克.com/baodian" },
+  { name: "Vue实践", href: "http://千克.com/vue" },
+  { name: "React实践", href: "http://千克.com/react" },
+  { name: "npm包源码解读", href: "http://千克.com/npm" }
 ]
 
 var network = [
-  {
-    name: "github",
-    href: "https://github.com"
-  },
-  {
-    name: "npm",
-    href: "https://www.npmjs.com"
-  },
-  {
-    name: "node中文",
-    href: "http://nodejs.cn/api"
-  },
-  {
-    name: "node官网",
-    href: "https://nodejs.org/zh-cn"
-  },
-  {
-    name: "sui mobile",
-    href: "https://sui.ctolog.com"
-  },
-  {
-    name: "layui",
-    href: "https://www.layui.com/doc"
-  }
+  { name: "github", href: "https://github.com" },
+  { name: "npm", href: "https://www.npmjs.com" },
+  { name: "node中文", href: "http://nodejs.cn/api" },
+  { name: "node官网", href: "https://nodejs.org/zh-cn" },
+  { name: "sui mobile", href: "https://sui.ctolog.com" },
+  { name: "layui", href: "https://www.layui.com/doc" }
 ]
