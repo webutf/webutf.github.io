840c3609bf359bdd7cc9aa4d50d4b7e5e667854d
webutf <919882905@qq.com>
2020-03-04 17:14:09 +0800

    升级

--- data.js
+++ data.js
@@ -16,23 +16,30 @@ var data = {
       { name: "node官网", href: "https://nodejs.org/zh-cn" },
       { name: "Express", href: "https://www.expressjs.com.cn" },
       { name: "Koa", href: "https://koa.bootcss.com" },
-      { name: "Egg org", href: "https://eggjs.org/zh-cn" },
-      { name: "Egg github", href: "https://eggjs.github.io/zh" },
+      { name: "Egg", href: "https://eggjs.github.io/zh" },
+    ]
+  },
+  web: {
+    name: "前端框架、库",
+    data: [
+      { name: "jQuery", href: "http://jquery.cuishifeng.cn" },
+      { name: "jQuery官网", href: "https://www.jquery123.com/" },
+      { name: "Vue", href: "https://cn.vuejs.org" },
+      { name: "React", href: "https://react.docschina.org" },
+      { name: "NuxtJS", href: "https://www.nuxtjs.cn" },
+      { name: "NextJS", href: "https://www.nextjs.cn" },
+      { name: "Preact", href: "https://preactjs.com" },
+      { name: "Preact中文", href: "https://preact.bootcss.com" },
     ]
   },
   network: {
     name: "网络资料",
     data: [
-      { name: "jQuery", href: "http://jquery.cuishifeng.cn" },
-      { name: "jQuery官网", href: "https://www.jquery123.com/" },
       { name: "Zepto", href: "http://www.zeptojs.cn" },
-      { name: "Vue", href: "https://cn.vuejs.org" },
-      { name: "React", href: "https://react.docschina.org" },
+      { name: "echarts", href: "https://www.echartsjs.com/zh/index.html" },
       { name: "Lodash", href: "https://www.lodashjs.com" },
       { name: "underscore", href: "http://underscorejs.org" },
       { name: "underscore中文", href: "https://underscorejs.bootcss.com" },
-      { name: "NuxtJS", href: "https://www.nuxtjs.cn" },
-      { name: "NextJS", href: "https://www.nextjs.cn" },
       { name: "Babel", href: "https://www.babeljs.cn" },
       { name: "Preact", href: "https://preactjs.com" },
       { name: "Preact中文", href: "https://preact.bootcss.com" },
@@ -42,6 +49,11 @@ var data = {
     name: "UI框架",
     data: [
       { name: "Bootstrap", href: "https://www.bootcss.com" },
+      { name: "Element", href: "https://element.eleme.cn/#/zh-CN" },
+      { name: "vux", href: "https://doc.vux.li/zh-CN" },
+      { name: "mint-ui", href: "http://mint-ui.github.io/docs/#/zh-cn2" },
+      { name: "EasyUI中文", href: "http://www.jeasyui.net" },
+      { name: "Ant Design", href: "https://ant.design/docs/react/introduce-cn" },
       { name: "sui mobile", href: "https://sui.ctolog.com" },
       { name: "layui", href: "https://www.layui.com/doc" },
     ]
@@ -51,13 +63,14 @@ var data = {
     data: [
       { name: "github", href: "https://github.com" },
       { name: "npm", href: "https://www.npmjs.com" },
+      { name: "Iconfont", href: "https://www.iconfont.cn" },
       { name: "BootCDN", href: "https://www.bootcdn.cn" },
       { name: "webpack", style: "1", href: "https://www.webpackjs.com" },
       { name: "gulp", href: "https://www.gulpjs.com.cn" },
       { name: "less", href: "https://less.bootcss.com" },
       { name: "sass", href: "https://www.sasscss.com" },
-      { name: "npm中文文档", href: "https://www.npmjs.cn" },
-      { name: "yarn", href: "https://yarn.bootcss.com" },
+      { name: "npm中文", href: "https://www.npmjs.cn" },
+      { name: "yarn中文", href: "https://yarn.bootcss.com" },
       { name: "parcel", href: "https://www.parceljs.cn" },
       { name: "rollup", href: "https://www.rollupjs.com" },
     ]
