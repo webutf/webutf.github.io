5d8048f9342f9135f2db4dba8cc71c53c87af37e
webutf <919882905@qq.com>
2020-03-04 15:43:18 +0800

    升级

--- data.js
+++ data.js
@@ -1,19 +1,41 @@
-var qianke = [
-  { name: "XUI框架", href: "http://千克.com/xui" },
-  { name: "js函数宝典", href: "http://千克.com/baodian" },
-  { name: "Vue实践", href: "http://千克.com/vue" },
-  { name: "React实践", href: "http://千克.com/react" },
-  { name: "npm包源码解读", href: "http://千克.com/npm" }
-]
-
-var network = [
-  { name: "jQuery", href: "http://jquery.cuishifeng.cn" },
-  { name: "Vue", href: "https://cn.vuejs.org" },
-  { name: "React", href: "https://react.docschina.org" },
-  { name: "github", href: "https://github.com" },
-  { name: "npm", href: "https://www.npmjs.com" },
-  { name: "node中文", href: "http://nodejs.cn/api" },
-  { name: "node官网", href: "https://nodejs.org/zh-cn" },
-  { name: "sui mobile", href: "https://sui.ctolog.com" },
-  { name: "layui", href: "https://www.layui.com/doc" }
-]
+var data = {
+  qianke: {
+    name: "个人网站笔记整理",
+    data: [
+      { name: "XUI框架", href: "http://千克.com/xui" },
+      { name: "js函数宝典", href: "http://千克.com/baodian" },
+      { name: "Vue实践", href: "http://千克.com/vue" },
+      { name: "React实践", href: "http://千克.com/react" },
+      { name: "npm包源码解读", href: "http://千克.com/npm" },
+    ]
+  },
+  network: {
+    name: "网络资料",
+    data: [
+      { name: "jQuery", href: "http://jquery.cuishifeng.cn" },
+      { name: "Vue", href: "https://cn.vuejs.org" },
+      { name: "NuxtJS", href: "https://www.nuxtjs.cn" },
+      { name: "React", href: "https://react.docschina.org" },
+      { name: "NextJS", href: "https://www.nextjs.cn" },
+      { name: "node中文", href: "http://nodejs.cn/api" },
+      { name: "node官网", href: "https://nodejs.org/zh-cn" },
+    ]
+  },
+  ui: {
+    name: "UI框架",
+    data: [
+      { name: "Bootstrap", href: "https://www.bootcss.com" },
+      { name: "sui mobile", href: "https://sui.ctolog.com" },
+      { name: "layui", href: "https://www.layui.com/doc" },
+    ]
+  },
+  tool: {
+    name: "工具",
+    data: [
+      { name: "webpack", style:"", href: "https://www.webpackjs.com" },
+      { name: "github", href: "https://github.com" },
+      { name: "npm", href: "https://www.npmjs.com" },
+      { name: "BootCDN", href: "https://www.bootcdn.cn" },
+    ]
+  }
+}
