772946946bcc64f11298b9bb7f140105019a7ac4
webutf <919882905@qq.com>
2020-03-04 16:28:30 +0800

    升级

--- data.js
+++ data.js
@@ -9,16 +9,27 @@ var data = {
       { name: "npm包源码解读", href: "http://千克.com/npm" },
     ]
   },
+  node: {
+    name: "node相关文档",
+    data: [
+      { name: "node中文", href: "http://nodejs.cn/api" },
+      { name: "node官网", href: "https://nodejs.org/zh-cn" },
+      { name: "Express", href: "https://www.expressjs.com.cn" },
+      { name: "Koa", href: "https://koa.bootcss.com" },
+      { name: "Egg org", href: "https://eggjs.org/zh-cn" },
+      { name: "Egg github", href: "https://eggjs.github.io/zh" },
+    ]
+  },
   network: {
     name: "网络资料",
     data: [
       { name: "jQuery", href: "http://jquery.cuishifeng.cn" },
+      { name: "jQuery官网", href: "https://www.jquery123.com/" },
+      { name: "Zepto", href: "https://zeptojs.bootcss.com" },
       { name: "Vue", href: "https://cn.vuejs.org" },
       { name: "NuxtJS", href: "https://www.nuxtjs.cn" },
       { name: "React", href: "https://react.docschina.org" },
       { name: "NextJS", href: "https://www.nextjs.cn" },
-      { name: "node中文", href: "http://nodejs.cn/api" },
-      { name: "node官网", href: "https://nodejs.org/zh-cn" },
     ]
   },
   ui: {
@@ -32,7 +43,11 @@ var data = {
   tool: {
     name: "工具",
     data: [
-      { name: "webpack", style:"", href: "https://www.webpackjs.com" },
+      { name: "webpack", style: "1", href: "https://www.webpackjs.com" },
+      { name: "gulp", href: "https://www.gulpjs.com.cn" },
+      { name: "less", href: "https://less.bootcss.com" },
+      { name: "sass", href: "https://www.sasscss.com" },
+      { name: "npm", href: "https://www.npmjs.com" },
       { name: "github", href: "https://github.com" },
       { name: "npm", href: "https://www.npmjs.com" },
       { name: "BootCDN", href: "https://www.bootcdn.cn" },
