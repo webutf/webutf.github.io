962f579820d3fa597ca9a0058fc25b0959cb8420
webutf <919882905@qq.com>
2020-03-04 10:47:41 +0800

    升级

---
+++ data.js
@@ -0,0 +1,19 @@
+var qianke = [
+  { name: "XUI框架", href: "http://千克.com/xui" },
+  { name: "js函数宝典", href: "http://千克.com/baodian" },
+  { name: "Vue实践", href: "http://千克.com/vue" },
+  { name: "React实践", href: "http://千克.com/react" },
+  { name: "npm包源码解读", href: "http://千克.com/npm" }
+]
+
+var network = [
+  { name: "jQuery", href: "http://jquery.cuishifeng.cn" },
+  { name: "Vue", href: "https://cn.vuejs.org" },
+  { name: "React", href: "https://react.docschina.org" },
+  { name: "github", href: "https://github.com" },
+  { name: "npm", href: "https://www.npmjs.com" },
+  { name: "node中文", href: "http://nodejs.cn/api" },
+  { name: "node官网", href: "https://nodejs.org/zh-cn" },
+  { name: "sui mobile", href: "https://sui.ctolog.com" },
+  { name: "layui", href: "https://www.layui.com/doc" }
+]
